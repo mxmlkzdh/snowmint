51287b3ba11d39f41968671483c77f1cf988bf2e
Max Malekzadeh <mxmlkzdh@gmail.com>
2024-09-11 21:10:36 -0400

    Add the main logic to spin up the server.

--- cmd/snowmint/main.go
+++ cmd/snowmint/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/mxmlkzdh/snowmint/config"
+	"github.com/mxmlkzdh/snowmint/internal/id"
+	"github.com/mxmlkzdh/snowmint/internal/tcp"
+)
 
+func main() {
+	cfg := config.LoadConfig()
+	uniqueIDGenerator, err := id.NewUniqueIDGenerator(cfg.DataCenterID, cfg.NodeID, cfg.Epoch)
+	if err != nil {
+		panic(err)
+	}
+	server := tcp.NewServer(cfg.Address, cfg.Port, uniqueIDGenerator)
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
