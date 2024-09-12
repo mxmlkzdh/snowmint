738a5b8b2a3ac3fe2fa6d6c7946e470c265b9b12
Max Malekzadeh <11231195+mxmlkzdh@users.noreply.github.com>
2024-09-12 16:09:25 -0400

    Update command line flags

--- config/config.go
+++ config/config.go
@@ -15,8 +15,8 @@ type Config struct {
 func LoadConfig() *Config {
 	address := flag.String("address", "localhost", "The address to bind to.")
 	port := flag.Int("port", 8080, "The port to bind to.")
-	dataCenterID := flag.Int("dataCenterID", 0, "The data center ID.")
-	nodeID := flag.Int("nodeID", 0, "The node ID.")
+	dataCenterID := flag.Int("datacenter", 0, "The data center ID.")
+	nodeID := flag.Int("node", 0, "The node ID.")
 	epoch := flag.Int("epoch", 0, "The epoch in milliseconds.")
 	flag.Parse()
 	return &Config{
